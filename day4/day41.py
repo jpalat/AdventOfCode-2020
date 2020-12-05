3005ef090d925de8dfc7eae175666b5b76b1c222
Jay Palat <jay@palat.net>
2020-12-05 10:14:58 -0500

    add batchparser.
    Return a list of dicts that contain field and value.
    Load values at blank lines and at end of file.

--- day4/day41.py
+++ day4/day41.py
@@ -2,7 +2,27 @@ def passportValidator():
     print('Hello')
 
 def batchparser(input):
-    print(input)
+    source = list(input)
+    passports = []
+    passport = {}
+    for index, line in enumerate(source):
+        if line == '\n':
+            print("reset")
+            if passport != {}:
+                passports.append(passport)
+                passport = {}
+        else:
+            field_array = line.strip().split(' ')
+            for f in field_array:
+                key, value = f.split(':')
+                passport[key] = value
+    passports.append(passport)
+    print(passports)
+    return passports
+            
+
+
+
 
 if __name__ == "__main__":
     fileInput = open('input.txt','r')
