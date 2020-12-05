8b5854fe7b11c86ad9ffe9201de9d9303e69ad9e
Jay Palat <jay@palat.net>
2020-12-04 23:03:37 -0500

    Account for new line and len.

--- day3/day3-1.py
+++ day3/day3-1.py
@@ -3,7 +3,7 @@ f = open('sample.txt')
 pos = 0
 count = 0
 for line in f:
-    line_array = list(line)
+    line_array = list(line.strip())
     if line_array[pos] == '.':
         line_array[pos] = 'O'
     else:
@@ -11,6 +11,6 @@ for line in f:
         count += 1
 
     print(''.join(line_array))
-    pos = (pos + 3) % len(line)
+    pos = (pos + 3) % (len(line) -1)
 
 print("Trees Encountered", count)
