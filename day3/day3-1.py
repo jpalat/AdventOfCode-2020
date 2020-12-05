83c925f0d2370d3946731d2209ac9d8ebb10a4d9
Jay Palat <jay@palat.net>
2020-12-04 22:55:20 -0500

    Accounting for trees.

--- day3/day3-1.py
+++ day3/day3-1.py
@@ -1,8 +1,16 @@
 f = open('sample.txt')
 
 pos = 0
+count = 0
 for line in f:
     line_array = list(line)
-    line_array[pos] = 'X'
+    if line_array == '.':
+        line_array[pos] = 'O'
+    else:
+        line_array[pos] = 'X'
+        count += 1
+
     print(''.join(line_array))
     pos = (pos + 3) % len(line)
+
+print("Trees Encountered", count)
