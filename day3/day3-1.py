c63d459d5099dc8d1d1e8e0bce3b16a55886188c
Jay Palat <jay@palat.net>
2020-12-04 22:49:49 -0500

    First pass plotting on grid.

--- day3/day3-1.py
+++ day3/day3-1.py
@@ -2,5 +2,7 @@ f = open('sample.txt')
 
 pos = 0
 for line in f:
-    line[pos] = 'X'
-    print(line)
+    line_array = list(line)
+    line_array[pos] = 'X'
+    print(line_array.join(''))
+    pos = (pos + 3) % len(line)
