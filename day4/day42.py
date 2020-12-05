220208f567b4be7e9d5144b9bdf7b21025b1f9f5
Jay Palat <jay@palat.net>
2020-12-05 11:58:27 -0500

    New files for Project 2. Testing fields.

---
+++ day4/day42.py
@@ -0,0 +1,43 @@
+def passportValidator(passport):
+    required_keys = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+    optional_keys = ['cid']
+    valid = True
+    for key in required_keys:
+        if key in passport:
+            pass
+        else:
+            return False
+    return True
+
+def batchparser(input):
+    source = list(input)
+    passports = []
+    passport = {}
+    for index, line in enumerate(source):
+        if line == '\n':
+            print("reset")
+            if passport != {}:
+                passports.append(passport)
+                passport = {}
+        else:
+            field_array = line.strip().split(' ')
+            for f in field_array:
+                key, value = f.split(':')
+                passport[key] = value
+    passports.append(passport)
+    print(passports)
+    return passports
+            
+
+def fieldValidator(key, value):
+    return None
+
+
+if __name__ == "__main__":
+    fileInput = open('input.txt','r')
+    passports = batchparser(fileInput)
+    count = 0
+    for passport in passports:
+        if passportValidator(passport):
+            count += 1
+    print("Valid Passports:", count)
---
+++ day4/day42_test.py
@@ -0,0 +1,54 @@
+import unittest
+from day42 import batchparser, passportValidator, fieldValidator
+
+class TestBatch(unittest.TestCase):
+    
+    def setUp(self):
+        self.f = open('sample.txt','r')
+    def tearDown(self):
+        self.f.close()
+
+    def test_batchExtractor(self):
+        self.assertEqual(len(batchparser(self.f)), 4, "4 batches")
+
+class TestValidation(unittest.TestCase):
+    def test_validPassport(self):
+        self.assertEqual(passportValidator({'ecl': 'gry', 'pid': '860033327', 'eyr': '2020', 'hcl': '#fffffd', 'byr': '1937', 'iyr': '2017', 'cid': '147', 'hgt': '183cm'}), True, "Valid")
+    def test_invalidPassport_missing_hgt(self):
+        self.assertEqual(passportValidator({'iyr': '2013', 'ecl': 'amb', 'cid': '350', 'eyr': '2023', 'pid': '028048884', 'hcl': '#cfa07d', 'byr': '1929'}), False, "invalid")
+    def test_invalidPassport_missing_cid(self):
+        self.assertEqual(passportValidator({'hcl': '#ae17e1', 'iyr': '2013', 'eyr': '2024', 'ecl': 'brn', 'pid': '760753108', 'byr': '1931', 'hgt': '179cm'}), True, "Valid")
+    def test_invalidPassport_missing_cid_byr(self):
+        self.assertEqual(passportValidator({'hcl': '#cfa07d', 'eyr': '2025', 'pid': '166559648', 'iyr': '2011', 'ecl': 'brn', 'hgt': '59in'}), False, "invalid")
+
+class TestFieldValidation(unittest.TestCase):
+    def test_byr_valid(self):
+        self.assertEqual(fieldValidator('byr','2002'), True, 'Valid')
+    def test_byr_invalid(self):
+        self.assertEqual(fieldValidator('byr','2003'), False, 'Invalid')
+    def test_hgt_valid_in(self):
+        self.assertEqual(fieldValidator('hgt','60in'), True, 'Valid')
+    def test_hgt_valid_cm(self):
+        self.assertEqual(fieldValidator('hgt','190cm'), True, 'Valid')
+    def test_hgt_invalid_measure(self):
+        self.assertEqual(fieldValidator('hgt','190in'), False, 'Invalid')
+    def test_hgt_invalid_units(self):
+        self.assertEqual(fieldValidator('hgt','190'), False, 'Invalid')
+    def test_hcl_valid(self):
+        self.assertEqual(fieldValidator('hcl','#123abc'), True, 'Valid')
+    def test_hcl_invalid_value(self):
+        self.assertEqual(fieldValidator('hcl','#123abz'), False, 'Invalid')
+    def test_hcl_invalid_ha(seshlf):
+        self.assertEqual(fieldValidator('hcl','123abc'), False, 'Invalid')
+    def test_ecl_valid(self):
+        self.assertEqual(fieldValidator('ecl','brn'), False, 'Valid')
+    def test_ecl_invalid(self):
+        self.assertEqual(fieldValidator('ecl','wat'), False, 'Invalid')
+    def test_pid_valid(self):
+        self.assertEqual(fieldValidator('pid','000000001'), True, 'Valid')
+    def test_pid_invalid(self):
+        self.assertEqual(fieldValidator('pid','0123456789'), False, 'Invalid')
+ 
+if __name__ == "__main__":
+    
+    unittest.main()
