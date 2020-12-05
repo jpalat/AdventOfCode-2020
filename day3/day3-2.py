0613b1a7b6f0b81814be8ee6909108206cd81e2f
Jay Palat <jay@palat.net>
2020-12-05 07:58:53 -0500

    Add tests to match the sample.

--- day3/day3-2.py
+++ day3/day3-2.py
@@ -1,9 +1,12 @@
-f = open('sample.txt')
+import unittest
 
-def tree_test(rise, run):
+
+
+def navigator(file_handle, rise, run):
     pos = 0
     count = 0
-    for index, line in enumerate(f):
+    print("rise, run", rise, run)
+    for index, line in enumerate(file_handle):
         if index % rise != 0:
             # print(index, rise, index % rise)
             print(line.strip())
@@ -16,8 +19,32 @@ def tree_test(rise, run):
                 count += 1
 
             print(''.join(line_array))
-            pos = (pos + 3) % (len(line) -1)
+            pos = (pos + run) % (len(line) -1)
+    print("Count", count)
     return count
 
-print("Trees Encountered", tree_test(2,3))
+class TestNavigator(unittest.TestCase):
+    
+    def setUp(self):
+        self.f = open('sample.txt','r')
+    def tearDown(self):
+        self.f.close()
 
+    def test_11(self):
+        self.assertEqual(navigator(self.f, 1 ,1), 2, "Should be 2")
+    def test_13(self):
+        self.assertEqual(navigator(self.f, 1 ,3), 7, "Should be 7")
+    def test_15(self):
+        self.assertEqual(navigator(self.f, 1 ,5), 3, "Should be 3")
+    def test_17(self):
+        self.assertEqual(navigator(self.f, 1 ,7), 4, "Should be 4")
+    def test_21(self):
+        self.assertEqual(navigator(self.f, 2 ,1), 2, "Should be 2")
+
+
+
+
+if __name__ == "__main__":
+    
+    unittest.main()
+    print("Trees Encountered", navigator(2,3))
