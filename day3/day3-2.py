c5f1c955333cb53e6332d3385e9923c9daa36653
Jay Palat <jay@palat.net>
2020-12-04 23:40:41 -0500

    Add support for rise > 1.

--- day3/day3-2.py
+++ day3/day3-2.py
@@ -3,16 +3,21 @@ f = open('sample.txt')
 def tree_test(rise, run):
     pos = 0
     count = 0
-    for line in f:
-        line_array = list(line.strip())
-        if line_array[pos] == '.':
-            line_array[pos] = 'O'
-        else:
-            line_array[pos] = 'X'
-            count += 1
+    for index, line in enumerate(f):
+        if index % rise != 0:
+            # print(index, rise, index % rise)
+            print(line.strip())
+        else: 
+            line_array = list(line.strip())
+            if line_array[pos] == '.':
+                line_array[pos] = 'O'
+            else:
+                line_array[pos] = 'X'
+                count += 1
 
-        print(''.join(line_array))
-        pos = (pos + 3) % (len(line) -1)
+            print(''.join(line_array))
+            pos = (pos + 3) % (len(line) -1)
     return count
 
-print("Trees Encountered", tree_test(1,3))
+print("Trees Encountered", tree_test(2,3))
+
