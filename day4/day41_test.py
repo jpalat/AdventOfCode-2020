15d8d83ab6736d6c47686b732a09904f13750e08
Jay Palat <jay@palat.net>
2020-12-05 11:42:01 -0500

    Test refactor and cleanup.

--- day4/day41_test.py
+++ day4/day41_test.py
@@ -1,7 +1,7 @@
 import unittest
 from day41 import batchparser, passportValidator
 
-class TestNavigator(unittest.TestCase):
+class TestBatch(unittest.TestCase):
     
     def setUp(self):
         self.f = open('sample.txt','r')
@@ -10,6 +10,8 @@ class TestNavigator(unittest.TestCase):
 
     def test_batchExtractor(self):
         self.assertEqual(len(batchparser(self.f)), 4, "4 batches")
+
+class TestValidation(unittest.TestCase):
     def test_validPassport(self):
         self.assertEqual(passportValidator({'ecl': 'gry', 'pid': '860033327', 'eyr': '2020', 'hcl': '#fffffd', 'byr': '1937', 'iyr': '2017', 'cid': '147', 'hgt': '183cm'}), True, "Valid")
     def test_invalidPassport_missing_hgt(self):
@@ -20,9 +22,6 @@ class TestNavigator(unittest.TestCase):
         self.assertEqual(passportValidator({'hcl': '#cfa07d', 'eyr': '2025', 'pid': '166559648', 'iyr': '2011', 'ecl': 'brn', 'hgt': '59in'}), False, "invalid")
 
 
-
-
 if __name__ == "__main__":
     
     unittest.main()
-    print("Trees Encountered", navigator(2,3))
