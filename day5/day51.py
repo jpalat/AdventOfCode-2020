d6d68edb30b81a79f79bb015fcddf6300320b9ea
Jay Palat <jay@palat.net>
2020-12-05 15:08:18 -0500

    Add seat decoder.

--- day5/day51.py
+++ day5/day51.py
@@ -1,2 +1,40 @@
+import struct
+import math
+
 def seatDecoder(passid):
-    return None
+    print('passid:', passid)
+    instructions = list(passid)
+    row_instructions = instructions[:7]
+    seat_instructions = instructions[-3:]
+    row = parseRow(row_instructions)
+    col = parseSeat(seat_instructions)
+    seat = (row * 8) + col
+    print(row_instructions, seat_instructions)
+    return (row, col, seat)
+
+def parseRow(instructions):
+    lb = 0
+    ub = 127
+    for f in list(instructions):
+        border = math.floor((ub-lb)/2) + lb
+        if f == 'F':
+            ub = border
+        else:
+            lb = border + 1
+        print(f, lb, ub, border)
+    return border
+
+def parseSeat(instructions):
+    lb = 0
+    ub = 7
+    for f in list(instructions):
+        border = math.floor((ub-lb)/2) + lb
+        if f == 'L':
+            ub = border
+        else:
+            lb = border + 1
+        print(f, lb, ub, border)
+    return lb
+
+        
+
