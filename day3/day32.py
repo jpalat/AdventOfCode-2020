787a76cbd7e390f7b4e6d540bc5290699ed32b5d
Jay Palat <jay@palat.net>
2020-12-05 09:41:20 -0500

    Runs the advent challenge.
    Pass in a list instead of the file handle.  Using the filehandle, the file is read
    by the first call to navigator and the
    rest return empty (the file has been read).

--- day3/day32.py
+++ day3/day32.py
@@ -23,3 +23,12 @@ def navigator(file_handle, rise, run):
     print("Count", count)
     return count
 
+if __name__ == "__main__":
+    fileInput = open('input.txt','r')
+    f = list(fileInput)
+    run11 = navigator(f, 1, 1)
+    run13 = navigator(f, 1, 3)
+    run15 = navigator(f, 1, 5)
+    run17 = navigator(f, 1, 7)
+    run21 = navigator(f, 2, 1)
+    print('Product:', run11 * run13 * run15 * run17 * run21)
