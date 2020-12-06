b7eda266116ea346e0abce70e45a4433c085e86a
Jay Palat <jay@palat.net>
2020-12-06 10:28:41 -0500

    Start Day 6.

---
+++ day6/day61_test.py
@@ -0,0 +1,17 @@
+import unittest
+from day61 import 
+
+class TestDeclarations(unittest.TestCase):
+    
+    def setUp(self):
+        self.f = open('sample.txt','r')
+    def tearDown(self):
+        self.f.close()
+
+    def test_(self):
+        self.assertEqual()
+    def test_(self):
+        self.assertEqual()
+
+if __name__ == "__main__":
+    unittest.main()
