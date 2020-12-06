72efa97e32cebf46a35b4342e73e3556d774f148
Jay Palat <jay@palat.net>
2020-12-05 22:53:56 -0500

    Simplify parse to binary calculation.
    Read the instruction backwards and calculate the integer value for each
    place.

--- day5/day51.py
+++ day5/day51.py
@@ -5,37 +5,27 @@ def seatDecoder(passid):
     print('passid:', passid)
     instructions = list(passid)
     row_instructions = instructions[:7]
-    seat_instructions = instructions[-3:]
+    col_instructions = instructions[-3:]
     row = parseRow(row_instructions)
-    col = parseCol(seat_instructions)
+    col = parseCol(col_instructions)
     seat = (row * 8) + col
     # print(row_instructions, seat_instructions)
     return (row, col, seat)
 
 def parseRow(instructions):
-    lb = 0
-    ub = 127
     row = 0
-    for f in list(instructions):
-        border = math.floor((ub-lb)/2) + lb
-        if f == 'F':
-            ub = border
-        else:
-            lb = border + 1
-        # print(f, lb, ub, border)
-    return border
+    for index, r in enumerate(reversed(instructions)):
+        if r == 'B':
+            row = (2**index) + row
+    return row
 
 def parseCol(instructions):
-    lb = 0
-    ub = 7
-    for f in list(instructions):
-        border = math.floor((ub-lb)/2) + lb
-        if f == 'L':
-            ub = border
-        else:
-            lb = border + 1
-        # print(f, lb, ub, border)
-    return lb
+    column = 0
+    for index,c in enumerate(reversed(instructions)):
+        if c == 'R':
+            column = (2**index) + column
+    return column
+            
 
 if __name__ == "__main__":
     f = open('input.txt')
