e28c3609241bd3e783ebaf35deb689318929b2d6
Jay Palat <jay@palat.net>
2020-12-05 22:34:00 -0500

    Add row test.

--- day5/day51_test.py
+++ day5/day51_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from day51 import seatDecoder
+from day51 import seatDecoder, parseRow, parseCol
 
 class TestSeatDecoder(unittest.TestCase):
     def test_1(self):
@@ -8,6 +8,8 @@ class TestSeatDecoder(unittest.TestCase):
         self.assertEqual(seatDecoder('BFFFBBFRRR'), (70, 7, 567))
         self.assertEqual(seatDecoder('FFFBBBFRRR'), (14, 7, 119))
         self.assertEqual(seatDecoder('BBFFBBFRLL'), (102, 4, 820))
+    def test_row(self):
+        self.assertEqual(parseRow('FBFBBFF'), 44)
 
 if __name__ == "__main__":
     unittest.main()
