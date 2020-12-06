7099dea95fad4a34de978538dfbc96ae85963145
Jay Palat <jay@palat.net>
2020-12-06 10:56:32 -0500

    Add test to verify each entry represents  a group.
    Each group of respondents is in a list and each person has a entry.

--- day6/day61_test.py
+++ day6/day61_test.py
@@ -10,8 +10,15 @@ class TestDeclarations(unittest.TestCase):
 
     def test_groups(self):
         self.assertEqual(len(getGroups(self.f)), 5, "5 groups found.")
+    def test_batch_contents(self):
+        groups = getGroups(self.f)
+        self.assertEqual(groups[0], ['abc'])
+        self.assertEqual(groups[1], ['a','b','c'])
+        self.assertEqual(groups[3], ['a','a','a','a'])
+
     # def test_(self):
     #     self.assertEqual()
+
 
 if __name__ == "__main__":
     unittest.main()
