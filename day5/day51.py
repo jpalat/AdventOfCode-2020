2e5566986443fcb4892f071a6f39bca42918d093
Jay Palat <jay@palat.net>
2020-12-05 22:35:27 -0500

    Look for highest id in input.txt.

--- day5/day51.py
+++ day5/day51.py
@@ -7,24 +7,25 @@ def seatDecoder(passid):
     row_instructions = instructions[:7]
     seat_instructions = instructions[-3:]
     row = parseRow(row_instructions)
-    col = parseSeat(seat_instructions)
+    col = parseCol(seat_instructions)
     seat = (row * 8) + col
-    print(row_instructions, seat_instructions)
+    # print(row_instructions, seat_instructions)
     return (row, col, seat)
 
 def parseRow(instructions):
     lb = 0
     ub = 127
+    row = 0
     for f in list(instructions):
         border = math.floor((ub-lb)/2) + lb
         if f == 'F':
             ub = border
         else:
             lb = border + 1
-        print(f, lb, ub, border)
+        # print(f, lb, ub, border)
     return border
 
-def parseSeat(instructions):
+def parseCol(instructions):
     lb = 0
     ub = 7
     for f in list(instructions):
@@ -33,8 +34,20 @@ def parseSeat(instructions):
             ub = border
         else:
             lb = border + 1
-        print(f, lb, ub, border)
+        # print(f, lb, ub, border)
     return lb
 
+if __name__ == "__main__":
+    f = open('input.txt')
+    l = list(f)
+    l.sort()
+    bigseat = 0
+    for index, seat in enumerate(l):
+        row, col, seat = seatDecoder(seat.strip())
+        print (index, ': ',row, col, seat, bigseat)
+        if seat > bigseat:
+            bigseat = seat
+        
+    print("Highest Seat ID:", bigseat)
         
 
