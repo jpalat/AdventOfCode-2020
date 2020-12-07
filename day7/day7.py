b3891e2ebdf4aa34ef3c1b0b0ab6f76538207caa
Jay Palat <jay@palat.net>
2020-12-07 08:44:25 -0500

    Working parser that gets the contents of the rule.

--- day7/day7.py
+++ day7/day7.py
@@ -1,2 +1,30 @@
 def parseRule(rule):
-    return None
+    results = splitParser(rule)
+    response = {}
+    if len(results) == 0:
+        return {}
+    else:
+        for r in results:
+            qty, mod, bag = r
+            display_name = mod + " " + bag
+            response[display_name] = int(qty)
+
+    return response
+
+def tokenParser(rule):
+    tokens = rule.split(' ')
+    print(tokens)
+
+def splitParser(rule):
+    head, tail = rule.split(' contain ')
+    container_list = tail.split(', ')
+    results = []
+    for c in container_list:
+        tokens = c.split(' ')
+        if len(tokens) > 3:
+            qty, mod, bag, _ = tokens
+            results.append((qty, mod, bag))
+    return results
+
+            
+        
--- day7/day7_test.py
+++ day7/day7_test.py
@@ -9,7 +9,9 @@ class TestLuggageRules(unittest.TestCase):
         self.f.close()
 
     def test_ruleParser(self):
-        self.assertEqual(parseRule('light red bags contain 1 bright white bag, 2 muted yellow bags.'), {'bright white bag':1, 'muted yellow bags':2})
+        self.assertEqual(parseRule('light red bags contain 1 bright white bag, 2 muted yellow bags.'), {'bright white':1, 'muted yellow':2})
+        self.assertEqual(parseRule('faded blue bags contain no other bags.'), {})
+        self.assertEqual(parseRule('bright white bags contain 1 shiny gold bag.'), {'shiny gold':1})
 
 
 if __name__ == "__main__":
