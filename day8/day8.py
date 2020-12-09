0460534826da6b56da0836315527911bcbf6de60
Jay Palat <jay@palat.net>
2020-12-08 21:36:29 -0500

    Mutate a program to get it end.
    * add a mutate function that will change nop -> op and op-nop  and then
    * add rule to
    execute the problem again;
    * update execute to reset accumulator and visted every time.
    * add a condition in execute to return if program completes.

--- day8/day8.py
+++ day8/day8.py
@@ -31,12 +31,47 @@ class BootLoader:
                         print("Unknown opp -")
                         print(operator)
                         exit(0)
-        
+            if index == len(self.program):
+                print('End of Program')
+                return "True"        
             if index in self.visited:
                 Halt = True
                 print('exiting')
                 return self.accumulator
 
+
+    def setRule(self, index, operator, operand):
+        newRule = operator + ' ' + str(operand) + "\n"
+        self.program[index] = newRule
+
+    def mutate(self):
+        index = 0
+        halt = False
+        program = self.program
+        for index, rule in enumerate(program):
+            self.visited = []
+            self.accumulator = 0
+            operator, operand = self.parseRule(index)
+            if operator == 'nop':
+                self.setRule(index,'jmp', operand)
+                res = self.execute()
+                if isinstance(res, str):
+                    return self.accumulator
+                else:
+                    self.setRule(index, operator, operand)
+            if operator == 'jmp':
+                self.setRule(index,'nop', operand)
+                res = self.execute()
+                if isinstance(res, str):
+                    return self.accumulator
+                else:
+                    self.setRule(index, operator, operand)              
+
+
+
+
+
+        
             
 if __name__ == "__main__":
     b = BootLoader('input.txt')
