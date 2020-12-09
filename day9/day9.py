8df2d9f781e5e3d16616b743853fc008a44566a1
Jay Palat <jay@palat.net>
2020-12-09 08:49:49 -0500

    Implement find and checkrange.
    Find will prep the data and set the window for checkrange.
    
    Checkrange will return true if it finds a pair of addends in the window
    that sum to target

--- day9/day9.py
+++ day9/day9.py
@@ -1,2 +1,23 @@
-def find(data, window):
+def find(input, window):
+    data = []
+    data = list(map(lambda x: int(x), input))
+    start = window 
+    for i in range(start, len(data)):
+        addSlice = slice(i - window, i,1)
+        print(data[i], data[addSlice])
+        if checkrange(data[i], data[addSlice]) == False:
+            return data[i]
     return 0
+
+
+
+'''
+Check the target is sum of 2 addends.
+'''
+def checkrange(target, addends):
+    result = False
+    for i in addends:
+        if target - i in addends:
+            result = True
+            return result
+    return result
