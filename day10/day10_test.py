90f4c44a015deaa2d0952c3c5561910b0c2c882c
Jay Palat <jay@palat.net>
2020-12-12 22:06:17 -0500

    Add second sample test.

--- day10/day10_test.py
+++ day10/day10_test.py
@@ -8,6 +8,11 @@ class TestDeclarations(unittest.TestCase):
         data = list(self.f)
         self.f.close()
         self.assertEqual(chain(data), {'1 jolt': 7, '3 jolts':5})
+    def test_chain2(self):
+        self.f = open('sample2.txt','r')
+        data = list(self.f)
+        self.f.close()
+        self.assertEqual(chain(data), {'1 jolt': 22, '3 jolts':10})
 
 if __name__ == "__main__":
     unittest.main()
