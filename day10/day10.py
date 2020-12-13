2e85eb22dfc69ddf6f1b0e5e3005b9e2a9156765
Jay Palat <jay@palat.net>
2020-12-12 22:05:02 -0500

    Add implementation for chain.

--- day10/day10.py
+++ day10/day10.py
@@ -1,2 +1,16 @@
 def chain(adapters):
-    return {}
+    adapter_list = sorted(list(map(lambda x: int(x), adapters)))
+    plug = 0
+    device = max(adapter_list) + 3
+    print(plug, adapter_list, device)
+    adapter_list.append(device)
+    last = 0
+    joltage = {'1 jolt':0, '3 jolts':0}
+    for i in adapter_list:
+        if i - last == 1:
+            joltage['1 jolt'] += 1
+        if i - last == 3:
+            joltage['3 jolts'] += 1
+        last = i
+    print (adapter_list)
+    return joltage
