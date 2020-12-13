82556cb55d0c1324be285c18e4eef9f4ee967936
Jay Palat <jay@palat.net>
2020-12-12 22:04:38 -0500

    refactor test.

--- day10/day10_test.py
+++ day10/day10_test.py
@@ -2,13 +2,11 @@ import unittest
 from day10 import chain
 class TestDeclarations(unittest.TestCase):
     
-    def setUp(self):
-        self.f = open('sample1.txt','r')
-    def tearDown(self):
-        self.f.close()
 
     def test_chain(self):
+        self.f = open('sample1.txt','r')
         data = list(self.f)
+        self.f.close()
         self.assertEqual(chain(data), {'1 jolt': 7, '3 jolts':5})
 
 if __name__ == "__main__":
