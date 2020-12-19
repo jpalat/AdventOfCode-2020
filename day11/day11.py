8a5fe1520bb9cce9cc5279565d10a9699c6d7371
Jay Palat <jay@palat.net>
2020-12-19 11:52:53 -0500

    Parse input and create floorplan.

--- day11/day11.py
+++ day11/day11.py
@@ -1,2 +1,8 @@
 def model_seats(input):
+    floor = list(input)
+    floor_plan = []
+    size = len(floor[0])-1
+    for l in floor:
+        line = list(l.strip())
+        floor_plan.append(line)
     return 0
