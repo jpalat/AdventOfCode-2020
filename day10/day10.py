f6a406e9b006db5bd9ce637ad1d144807b7f2cf6
Jay Palat <jay@palat.net>
2020-12-19 10:44:13 -0500

    Remove prints.

--- day10/day10.py
+++ day10/day10.py
@@ -2,7 +2,6 @@ def chain(adapters):
     adapter_list = sorted(list(map(lambda x: int(x), adapters)))
     plug = 0
     device = max(adapter_list) + 3
-    print(plug, adapter_list, device)
     adapter_list.append(device)
     last = 0
     joltage = {'1 jolt':0, '3 jolts':0}
@@ -12,7 +11,6 @@ def chain(adapters):
         if i - last == 3:
             joltage['3 jolts'] += 1
         last = i
-    print (adapter_list)
     return joltage
 
 def arrangements(data):
